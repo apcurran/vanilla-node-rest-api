c0175e2e2a37848e9a3abc92efb3c7d5552198fd
Alex Curran <alexcurran.va@gmail.com>
2021-02-12 12:05:05 -0600

    Initial server setup and one api route serving JSON data for products

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const http = require("http");
+const PORT = process.env.PORT || 5000;
+const products = require("./data/products.json");
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/api/products") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(products));
+    } else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Route not found" }));
+    }
+});
+
+server.listen(PORT, () => console.log(`Server listening on port, ${PORT}`));
